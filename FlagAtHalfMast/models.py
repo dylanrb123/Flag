d017ea14fbfad7d5c30bde31bca1a08b8718b811
Dylan Bannon <drb2857@rit.edu>
2016-03-25 09:42:34 -0400

    added models

--- FlagAtHalfMast/models.py
+++ FlagAtHalfMast/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FlagHalfMastInfo(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason = models.CharField(255)
+
